22e431b4691356d30e53428104d64b7e6141de38
Andrew Lamagna <andrew.lamagna@LEGO.com>
2022-05-12 16:02:55 -0400

    Basic stuff started

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header" />
+      {/* <header className="App-header" /> */}
+      <h1>Scroll down</h1>
+      <div className="trigger">
+        <div className="hand-container">
+          <div className="lefthand" />
+          <div className="righthand" />
+        </div>
+      </div>
+      <div>
+        <p>Some stuff?</p>
+      </div>
     </div>
   );
 }
