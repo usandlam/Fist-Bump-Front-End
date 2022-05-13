ba2036031d2eed16debe786f0696bc8c7250886b
usandlam <94059162+usandlam@users.noreply.github.com>
2022-05-13 10:40:38 -0400

    Bumpin and dumpin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,49 @@ function Navbar() {
 
   return (
     <div className="container text-center">
-      <nav className="starter-nav">
-        <Link to="/">
-          <button>Home</button>
+      <nav className="menu">
+        <input
+          type="checkbox"
+          href="#"
+          className="menu-open"
+          name="menu-open"
+          id="menu-open"
+        />
+        <label className="menu-open-button" htmlFor="menu-open">
+          <span className="lines line-1" />
+          <span className="lines line-2" />
+          <span className="lines line-3" />
+        </label>
+
+        <a href="#" className="menu-item blue">
+          {" "}
+          <i className="fa fa-anchor" />{" "}
+        </a>
+        <a href="#" className="menu-item green">
+          {" "}
+          <i className="fa fa-coffee" />{" "}
+        </a>
+        <a href="#" className="menu-item red">
+          {" "}
+          <i className="fa fa-heart" />{" "}
+        </a>
+        <a href="#" className="menu-item purple">
+          {" "}
+          <i className="fa fa-microphone" />{" "}
+        </a>
+        <a href="#" className="menu-item orange">
+          {" "}
+          <i className="fa fa-star" />{" "}
+        </a>
+        <a href="#" className="menu-item lightblue">
+          {" "}
+          <i className="fa fa-diamond" />{" "}
+        </a>
+      </nav>
+
+      <div className="col simple-nav d-flex align-items-center justify-content-evenly pt-1">
+        <Link className="btn btn-outline-info" to="/">
+          Home
         </Link>
 
         {isLoggedIn && (
@@ -23,15 +63,15 @@ function Navbar() {
 
         {!isLoggedIn && (
           <>
-            <Link to="/signup">
-              <button>Sign Up</button>
+            <Link className="btn btn-outline-info" to="/signup">
+              Sign Up
             </Link>
-            <Link to="/login">
-              <button>Login</button>
+            <Link className="btn btn-outline-info" to="/login">
+              Login
             </Link>
           </>
         )}
-      </nav>
+      </div>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,84 @@
+import { useRef, useEffect } from "react";
+
+import gsap from "gsap";
+
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const API_URL = "http://localhost:5005/api";
+
 function HomePage() {
+  const circleRef = useRef(null);
+
+  useEffect(() => {
+    const tl = gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: ".trigger",
+          pin: false,
+          start: "top top",
+          end: "+=1000",
+          scrub: true,
+          markers: false,
+          toggleClass: "enable",
+          // onLeave: () => gsap.set(".lefthand", { position: "absolute fixed" }),
+          onLeave: () => componentDidMount(),
+        },
+      })
+      .add("start")
+      .to(".lefthand", { xPercent: 250, duration: 4 }, "start")
+      .to(".righthand", { xPercent: -250, duration: 4 }, "start");
+  }, []);
+
+  const postABump = async (latitude, longitude) => {
+    const requestBody = { latitude, longitude };
+    console.log(requestBody);
+    try {
+      console.log("attempting bump");
+      const tryBump = await fetch(`${API_URL}/dap`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      });
+      const response = await tryBump.json();
+      console.log(response);
+      console.log("Bump uploaded w/ coordinates");
+    } catch (error) {
+      const errorDescription = error.response.message;
+      console.log(errorDescription);
+    }
+  };
+
+  const componentDidMount = () => {
+    if ("geolocation" in navigator) {
+      console.log("Available");
+
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position);
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        console.log("Latitude: ", lat);
+        console.log("Longitude: ", lng);
+
+        postABump(lat, lng);
+      });
+    }
+    console.log("Not Available");
+  };
+
   return (
-    <div className="container">
-      <h1>Scroll down</h1>
+    <div className="container text-center">
+      <h1>Scroll down to bump!</h1>
       <div className="trigger">
         <div className="hand-container">
           <div className="lefthand" />
           <div className="righthand" />
         </div>
       </div>
+
       <div />
     </div>
   );
